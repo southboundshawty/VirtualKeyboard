b613d742731d30b61c602985a1600517261944ef
southboundshawty <stevemine71@gmail.com>
2021-03-08 16:59:21 +0300

    Новая логика для лонг-пресса

--- VKBoard/VKeyboard/Views/Keys/VKeyboardKeyControl.xaml.cs
+++ VKBoard/VKeyboard/Views/Keys/VKeyboardKeyControl.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-using VKBoard.VKeyboard.Commands;
 using VKBoard.VKeyboard.Services;
 
 namespace VKBoard.VKeyboard.Views.Keys
@@ -18,9 +16,46 @@ namespace VKBoard.VKeyboard.Views.Keys
         public VKeyboardKeyControl()
         {
             InitializeComponent();
+
+            timer = new Timer
+            {
+                Enabled = false,
+                Interval = 100
+            };
+
+            timer.Elapsed += Timer_Elapsed;
         }
 
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                if (pressTime >= holdKeyTime)
+                {
+                    AlternativeSymbolVisibility = Visibility.Visible;
+
+                    timer.Enabled = false;
+
+                    pressTime = 0;
+
+                    return;
+                }
+                else
+                {
+                    AlternativeSymbolVisibility = Visibility.Collapsed;
+                }
+
+                pressTime += 100;
+            });
+
+        }
+
+        private const int holdKeyTime = 300;
+
         private DateTime pressedDateTime;
+
+        private readonly Timer timer;
+        private int pressTime = 0;
 
         public char Symbol
         {
@@ -42,24 +77,40 @@ namespace VKBoard.VKeyboard.Views.Keys
         public static readonly DependencyProperty AlternativeSymbolProperty =
             DependencyProperty.Register("AlternativeSymbol", typeof(char), typeof(VKeyboardKeyControl), new PropertyMetadata(default));
 
+        public Visibility AlternativeSymbolVisibility
+        {
+            get => (Visibility)GetValue(AlternativeSymbolVisibilityProperty);
+            set => SetValue(AlternativeSymbolVisibilityProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for AlternativeSymbolVisibility.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AlternativeSymbolVisibilityProperty =
+            DependencyProperty.Register("AlternativeSymbolVisibility", typeof(Visibility), typeof(VKeyboardKeyControl), new PropertyMetadata(Visibility.Collapsed));
+
         private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             pressedDateTime = DateTime.Now;
 
-            alternativeSymbolPopUp.IsOpen = AlternativeSymbol != '\0';
+            timer.Enabled = true;
+
+            //alternativeSymbolPopUp.IsOpen = AlternativeSymbol != '\0';
+            alternativeSymbolPopUp.IsOpen = true;
         }
 
         private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
         {
-            if (DateTime.Now.Subtract(pressedDateTime).TotalMilliseconds > 300)
-            {
+            timer.Enabled = false;
 
+            if (DateTime.Now.Subtract(pressedDateTime).TotalMilliseconds > holdKeyTime)
+            {
                 VKeyboardOperationsService.Instance.PressKey(AlternativeSymbol);
             }
             else
             {
                 VKeyboardOperationsService.Instance.PressKey(Symbol);
             }
+
+            AlternativeSymbolVisibility = Visibility.Collapsed;
 
             alternativeSymbolPopUp.IsOpen = false;
         }
